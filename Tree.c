5ae91200b9684def89b0fa4c27d913320928bac7
Dev-Tushar-sh <93375756+Dev-Tushar-sh@users.noreply.github.com>
2021-10-29 16:31:42 +0530

    Add files via upload

---
+++ Tree.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct Binarynode
+{
+    int info;
+    struct node *r;
+    struct node *l;
+};
+typedef struct Binarynode node;
+node *root=NULL;
+node* create_node()
+{
+    node *p;
+    p=(node *)malloc(sizeof(node));
+    printf("Enter the element : ");
+    scanf("%d",&p->info);
+    p->r=NULL;
+    p->l=NULL;
+    return(p);
+}
+node* create_node();
+void insertnode();
+void showlist();
+void main()
+{
+    while(1)
+    {
+        system("cls");
+
+        insertnode();
+        showlist();
+
+
+    }
+
+
+}
+
+
+void insertnode()
+{
+
+    node *temp,*tp;
+    temp=create_node();
+    if(root==NULL)
+       root=temp;
+    else
+    {
+        tp=root;
+        while(tp!=NULL)
+        {
+       if(tp->info>temp->info)
+       {
+           if(tp->l==NULL)
+            tp->l=temp;
+           tp=tp->l;
+       }
+       else if(tp->info<temp->info)
+       {
+         if(tp->r==NULL)
+            tp->r=temp;
+         tp=tp->r;
+
+       }
+       else
+         {
+             break;
+         }
+        }
+    }
+}
+void showlist()
+{
+    node *temp;
+    temp=root;
+    if(root==NULL)
+        printf("list is empty");
+    else
+    while(temp!=NULL)
+    {
+      printf(" %d ",temp->info);
+      temp=temp->r;
+    }
+    getch();
+
+
+}
+
+
+
+
+
+
